397a3b4e1def51851320f79c003b4c0719b8eb84
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2026-09-09 23:46:48 +0530

    binary search question push

--- 04_BinarySearch/1_binary_search.py
+++ 04_BinarySearch/1_binary_search.py
@@ -1,28 +1,61 @@
-def binary_search(nums, target):
+# #binary Search :- always in sorted manner
 
-    left = 0
-    right = len(nums) - 1
+# def binarySearch(nums,target):
 
-    while left <= right:
+#     n = len(nums)
 
-        mid = left + (right - left) // 2
+#     left = 0
+#     right = n - 1
 
-        if nums[mid] == target:
-            return mid
+#     while left <= right:
 
-        elif nums[mid] < target:
-            left = mid + 1
+#         mid = left + (right - left)//2
 
-        else:
-            right = mid - 1
+#         if nums[mid] == target:
+#             return mid
+        
+#         elif nums[mid] < target:
+#             left = mid + 1
 
-    return -1
+#         else:
+#             right = mid - 1
+
+    
+#     return -1
+
+# nums = [1,2,3,4,5,8]
+# target = 1
+# result = binarySearch(nums,target)
+# print(result)
 
 
-# Input
-nums = [1,2,3,4,5,6]
-target = 4
 
-result = binary_search(nums, target)
+##by using recursive method
 
-print(result)
+def binarySearch(nums,target,left,right):
+
+    #edge case
+    if left > right:
+        return -1
+    
+    mid = left + (right - left)//2
+
+    if nums[mid] == target:
+
+        return mid
+    
+    elif nums[mid] < target:
+        
+        return binarySearch(nums,target,mid+1,right)
+    
+    else:
+
+        return binarySearch(nums,target,left,mid-1)
+    
+
+nums = [1,2,3,4,5,8]
+left = 0
+right = len(nums) - 1
+target = 1
+result = binarySearch(nums,target,left,right)
+print(result)    
