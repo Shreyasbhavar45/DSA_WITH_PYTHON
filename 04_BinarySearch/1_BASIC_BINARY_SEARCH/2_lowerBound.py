c014b0f2eee44f3774b81b9891e714f770ebf7d3
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2026-09-26 15:39:17 +0530

    revision of upper bound and lower bound

--- 04_BinarySearch/1_BASIC_BINARY_SEARCH/2_lowerBound.py
+++ 04_BinarySearch/1_BASIC_BINARY_SEARCH/2_lowerBound.py
@@ -1,37 +1,71 @@
-## lower Bound :- Smallest index such that nums[i] >= target
+# ## lower Bound :- Smallest index such that nums[i] >= target
 
+
+# def LowerBound(nums,target):
+
+#     n = len(nums)
+
+#     lowerBound = n
+
+#     low = 0
+
+#     high = n - 1
+
+#     while low <= high:
+
+#         mid = low + (high - low)//2
+
+#         if nums[mid] >= target:
+            
+#             lowerBound = mid
+
+#             high = mid - 1
+
+#         else:
+
+#             low = mid + 1
+
+#     return lowerBound
+
+
+# nums = [1,1,1,2,2,3,3,4,5,6]
+
+# target = 1
+
+# result = LowerBound(nums,target)
+
+# print('The Lower Bound is :-',result)
+
+
+
+### 
 
 def LowerBound(nums,target):
 
     n = len(nums)
 
-    lowerBound = n
+    lowerb = n
 
-    low = 0
+    left = 0
+    right = n - 1
 
-    high = n - 1
+    while left <= right:
 
-    while low <= high:
-
-        mid = low + (high - low)//2
+        mid = left + (right - left)//2
 
         if nums[mid] >= target:
-            
-            lowerBound = mid
+            lowerb = mid
 
-            high = mid - 1
+            right = mid - 1
 
         else:
+            left = mid + 1
 
-            low = mid + 1
+    
+    return lowerb
 
-    return lowerBound
 
-
-nums = [1,1,1,2,2,3,3,4,5,6]
-
-target = 1
-
+nums = [1,2,2,3,4,4]
+target = 2
 result = LowerBound(nums,target)
-
-print('The Lower Bound is :-',result)
+print(result)
--- 04_BinarySearch/1_BASIC_BINARY_SEARCH/3_UpperBound.py
+++ 04_BinarySearch/1_BASIC_BINARY_SEARCH/3_UpperBound.py
@@ -5,26 +5,23 @@ def UpperBound(nums,target):
 
     n = len(nums)
 
-    upperbound = n
-
     low = 0
 
     high = n - 1
 
-    while low <= high:
+    while low < high:
 
         mid = low + (high - low)//2
 
         if nums[mid] > target:
-            upperbound = mid
 
-            high = mid - 1
+            high = mid
 
         else:
 
             low = mid + 1
 
-    return upperbound
+    return low
 
 
 nums = [1,1,1,2,2,3,3,4,5,6]
